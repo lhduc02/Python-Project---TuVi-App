c1be42855aa11365f23fe7a6761fba81d6e0825c
Lê Huỳnh Đức <76842196+lhduc02@users.noreply.github.com>
2024-04-13 10:31:07 +0700

    Update hoang_oc.py

--- hoang_oc.py
+++ hoang_oc.py
@@ -7,5 +7,3 @@ def pham_hoang_oc(year_birth, year):
     age2 = age[-1]
     hoangOc = ['Nhất Cát', 'Nhì Nghi', 'Tam Địa sát', 'Tứ Tấn tài', 'Ngũ Thọ tử', 'Lục Hoang ốc']
     return hoangOc[(int(age1)+int(age2))%6 - 1]
-
-print(pham_hoang_oc(2002, 2038))
