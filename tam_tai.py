53ecb5ac68934ea9768e4b4955ffeefb98877754
Lê Huỳnh Đức <76842196+lhduc02@users.noreply.github.com>
2024-10-19 10:12:06 +0700

    Update tam_tai.py

--- tam_tai.py
+++ tam_tai.py
@@ -2,7 +2,6 @@ def pham_tam_tai(year_birth, year):
     tuoi_12_con_giap = ['Tý', 'Sửu', 'Dần', 'Mão', 'Thìn', 'Tỵ', 'Ngọ', 'Mùi', 'Thân', 'Dậu', 'Tuất', 'Hợi']
     tuoi_sinh = tuoi_12_con_giap[(year_birth - 1900)%12]
     nam_hien_tai = tuoi_12_con_giap[(year - 1900)%12]
-    print(tuoi_sinh, nam_hien_tai)
     if tuoi_sinh in ['Tý', 'Thìn', 'Thân'] and nam_hien_tai in ['Dần', 'Mão', 'Thìn']:
         return '1'
     elif tuoi_sinh in ['Mão', 'Mùi', 'Hợi'] and nam_hien_tai in ['Tỵ', 'Ngọ', 'Mùi']:
